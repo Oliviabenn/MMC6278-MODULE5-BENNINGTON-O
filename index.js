167ce49eb821498e3896f7672a371d60fa8a62d6
Olivia <oliviabennington@gmail.com>
2022-06-07 21:08:13 -0400

    pseudocode

---
+++ index.js
@@ -0,0 +1,7 @@
+var form = document.querySelector('form')
+
+form.onClick = function(){}
+
+// Access form data by targeting input
+// Prevent form Submission
+// Reset form fields after console.logging data
